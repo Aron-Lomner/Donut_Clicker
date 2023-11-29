f1409136e923f1265555a8ac5624e502e8a2b02d
ceaselessphantom <90485964+ceaselessphantom@users.noreply.github.com>
2023-11-29 11:39:08 -0500

    made objects in object gameState.

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const clickPrice = 10;
-const multiplierPrice = 10;
 var gameState = {
   donuts: 0,
-  clickers: 0,
-  multipliers: 0,
+  clickers: = {
+    count: 0,
+    price: 1
+  },
+  multipliers: = {
+    count: 0,
+    price: 1
+  },
 };
 
 const donut = document.getElementById("donut");
@@ -11,18 +15,17 @@ const donutCount = document.getElementById("donut_count");
 const buyClickBtn = document.getElementById("buy_click_btn");
 const buyMultiplierBtn = document.getElementById("buy_multiplier_btn");
 const clickerCount = document.getElementById("clicker_count");
-const multiplyCount = document.getElementById("mutiply_count");
+const multiplyCount = document.getElementById("multiply_count");
 
 function updateDonutCount(clicks) {
   gameState.donuts += clicks * (1 + 1.2 ** multipliers);
   donutCount.innerText = `${gameState.donuts} donuts`;
 }
 function buy(item) {
-  if (item === click) {
-  }
-  if (gameState.donuts >= clickPrice) {
+  if (gameState.donuts >= item.price) {
     gameState.clickers += 1;
     gameState.donuts -= clickPrice;
+    item.price*=1.1;
   } else {
     // notEnoughMonyAnimate()
   }
